c87dfc1ff640642b98276f89e962762ed6a3d054
Envoy49 <envoy49@yahoo.com>
2023-11-02 08:02:45 +1100

    change the clone url

--- homebrew-go-spotify-cli/go-spotify-cli.rb
+++ homebrew-go-spotify-cli/go-spotify-cli.rb
@@ -1,6 +1,6 @@
 class MySpotifyCli < Formula
   desc "Go Spotify Cli"
-  homepage "https://github.com/envoy49/go-spotify-cli"
+  homepage "git@github.com:Envoy49/go-spotify-cli.git"
   url "https://github.com/envoy49/go-spotify-cli/gsc/gsc.tar.gz"
   sha256 "281c283a7cecda6773b3fb9f9ac236e43ecdc3c085bd37cf3ff3f72b9f5fdd56"
 

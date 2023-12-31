410276bc25b0d28c2accb97f4c365f6c968239c2
Envoy49 <envoy49@yahoo.com>
2023-12-31 16:25:33 +1100

    fix file location for powershel script

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,5 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir '.'
-
-$binPath = Join-Path (Get-ToolsLocation) '.'
+$fileLocation = Join-Path $toolsDir 'go-spotify-cli.exe'
+$binPath = Join-Path (Get-ToolsLocation) 'go-spotify-cli.exe'
 Copy-Item $fileLocation $binPath

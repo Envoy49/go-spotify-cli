c42b4b50471eecff884339870276a385f909f9c5
Envoy49 <envoy49@yahoo.com>
2024-01-01 11:32:38 +1100

    fix choco syntax error

--- commands/player/add_to_queue.go
+++ commands/player/add_to_queue.go
@@ -4,7 +4,6 @@ import (
 	"github.com/sirupsen/logrus"
 	"go-spotify-cli/commands"
 	"go-spotify-cli/constants"
-	"go-spotify-cli/types"
 )
 
 func AddToQueue(accessToken string, url string) {
@@ -16,15 +15,7 @@ func AddToQueue(accessToken string, url string) {
 	_, err := commands.Fetch(params)
 
 	if err != nil {
-		switch e := err.(type) {
-		case types.SpotifyAPIError:
-			if e.Detail.Error.Message == "Player command failed: No active device found" {
-				Device()
-			}
-		default:
-			logrus.WithError(err).Error("Error adding item to the queue")
-			return
-		}
-
+		logrus.WithError(err).Error("Error adding item to the queue")
+		return
 	}
 }

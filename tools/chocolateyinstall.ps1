367d4b4aa633d92355c9634447caf5f548c4e6d9
Envoy49 <envoy49@yahoo.com>
2024-01-01 23:07:48 +1100

    get rid of chocolatey

--- tools/chocolateyinstall.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = 'Stop';
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir '.\go-spotify-cli.exe'
-$binPath = Join-Path (Get-ToolsLocation) '.\go-spotify-cli.exe'
-Copy-Item $fileLocation $binPath
